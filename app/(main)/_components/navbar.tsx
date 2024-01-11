b2f703f295294239d50eb62b56766a1be182f502
Sandeep <sandy6862g22@gmail.com>
2024-01-11 22:28:05 +0530

    created a banner components and showing this only in archived docs

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { MenuIcon } from 'lucide-react';
 import Title from './title';
+import Banner from './banner';
 
 
 interface NavbarProps {
@@ -33,12 +34,17 @@ const Navbar = ({ isCollapse, onResetWidth }: NavbarProps) => {
     }
 
     return (
+        <>
         <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4'>
             {isCollapse && ( <MenuIcon className='h-6 w-6 text-muted-foreground' onClick={onResetWidth} role='button' />)}
             <div className='flex items-center justify-between w-full'>
                 <Title initialData={document} />
             </div>
         </nav>
+        {document.isArchived && (
+            <Banner documentId={document._id}  />
+        )}
+        </>
     )
 }
 
