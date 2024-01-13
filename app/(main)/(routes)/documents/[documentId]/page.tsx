486794de27b370b783baa6fd298e8fa89e3d115b
Sandeep <sandy6862g22@gmail.com>
2024-01-13 21:05:49 +0530

    created a toolbar for adding the icon and removing the icon

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client";
 
-const DocumentIdPage = () => {
+import React from 'react'
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import Toolbar from '@/components/toolbar';
+
+interface DocumentIdPageProps{
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({params}: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {documentId: params.documentId})
+
+  if(document === undefined){
+    return <p>loading...</p>
+  }
+
+  if(document === null){
+    return <div>Not found..</div>
+  }
+
+
   return (
-    <div>DocumentIdPage</div>
+    <div className='pb-40'>
+      <div className='h-[35vh]' />
+      <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <Toolbar initialData={document} />
+      </div>
+    </div>
   )
 }
 
