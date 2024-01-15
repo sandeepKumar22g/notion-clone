e7813b7dda54d7154944a23aef7d553d1e80bdaf
Sandeep <sandy6862g22@gmail.com>
2024-01-15 23:01:00 +0530

    created the skeleton of the cover

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import { api } from '@/convex/_generated/api';
 import { useParams } from 'next/navigation';
 import { Id } from '@/convex/_generated/dataModel';
 import { useEdgeStore } from '@/lib/edgestore';
+import { Skeleton } from './ui/skeleton';
 
 interface CoverImageProp{
     url?: string;
@@ -56,3 +57,9 @@ const Cover = ({preview, url}: CoverImageProp) => {
 }
 
 export default Cover
+
+Cover.Skeleton = function CoverSkeleton() {
+    return (
+        <Skeleton className='w-full h-[12vh]' />
+    )
+}
