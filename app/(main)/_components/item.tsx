4b90b3d9921ac615e41d035964cddfec398bd419
Sandeep <sandy6862g22@gmail.com>
2024-01-18 09:27:14 +0530

    fixed the icons alignment

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -81,7 +81,7 @@ const Item = ({id, label, icon: Icon, onClick, documentIcon, active, expanded, i
         { documentIcon ? (
             <div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>
         ): (
-            <Icon className='shrink-0 h-[18px] mr-2 text-muted-foreground' />
+            <Icon className='shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground' />
         )}
         <span className='truncate'>
             {label}
